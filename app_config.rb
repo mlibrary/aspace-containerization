69954df13c1823c8785498fc3fbe2ced9a225888
Greg Kostin <gkostin@umich.edu>
2023-05-04 13:28:33 -0400

    Use ENV variables in config.rb

--- app_config.rb
+++ app_config.rb
@@ -1,18 +1,18 @@
-AppConfig[:db_url] = "jdbc:mysql://db:3306/archivesspace?user=as&password=as123&useUnicode=true&characterEncoding=UTF-8"
-AppConfig[:solr_url] = "http://solr:8983/solr/archivesspace"
+AppConfig[:db_url] = ENV.fetch('DB_URL')
+AppConfig[:solr_url] = ENV.fetch('SOLR_URL')
 
-AppConfig[:backend_url] = "http://aspace:8089"
-AppConfig[:frontend_url] = "http://aspace:8080"
-AppConfig[:public_url] = "http://aspace:8081"
-AppConfig[:oai_url] = "http://aspace:8082"
-AppConfig[:indexer_url] = "http://aspace:8091"
-AppConfig[:docs_url] = "http://aspace:8888"
+AppConfig[:backend_url] = "#{ENV.fetch('HOST_URL')}:8089"
+AppConfig[:frontend_url] = "#{ENV.fetch('HOST_URL')}:8080"
+AppConfig[:public_url] = "#{ENV.fetch('HOST_URL')}:8081"
+AppConfig[:oai_url] = "#{ENV.fetch('HOST_URL')}:8082"
+AppConfig[:indexer_url] = "#{ENV.fetch('HOST_URL')}:8091"
+AppConfig[:docs_url] = "#{ENV.fetch('HOST_URL')}:8888"
 
-AppConfig[:frontend_log] = "/archivesspace/logs/archivesspace.out"
-AppConfig[:frontend_log_level] = "warn"
-AppConfig[:backend_log] = "/archivesspace/logs/archivesspace.out"
-AppConfig[:backend_log_level] = "warn"
-AppConfig[:pui_log] = "/archivesspace/logs/archivesspace.out"
-AppConfig[:pui_log_level] = "info"
-AppConfig[:indexer_log] = "/archivesspace/logs/archivesspace.out"
-AppConfig[:indexer_log_level] = "info"
+AppConfig[:frontend_log] = '/archivesspace/logs/archivesspace.out'
+AppConfig[:frontend_log_level] = 'warn'
+AppConfig[:backend_log] = '/archivesspace/logs/archivesspace.out'
+AppConfig[:backend_log_level] = 'warn'
+AppConfig[:pui_log] = '/archivesspace/logs/archivesspace.out'
+AppConfig[:pui_log_level] = 'info'
+AppConfig[:indexer_log] = '/archivesspace/logs/archivesspace.out'
+AppConfig[:indexer_log_level] = 'info'
