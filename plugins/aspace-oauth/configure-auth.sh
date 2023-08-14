45dd69510703a7eef5df5b0aa4e80e9e67b95361
Samuel Sciolla <ssciolla@umich.edu>
2023-08-14 09:49:05 -0400

    Tidy up plugin files, .gitignore

--- plugins/aspace-oauth/configure-auth.sh
+++ plugins/aspace-oauth/configure-auth.sh
@@ -9,8 +9,4 @@ git clone --depth 1 --branch v3.2.0 https://github.com/lyrasis/aspace-oauth.git
 cd /archivesspace/
 cat extra_gems.txt >> "$PLUGINS_DIR/aspace-oauth/Gemfile"
 
-# sed - i "s/gem 'addressable',   '~> 2.8'/gem 'addressable', "2.8.0"
-
 ./scripts/initialize-plugin.sh aspace-oauth
-
-cat "$PLUGINS_DIR/aspace-oauth/Gemfile.lock"
