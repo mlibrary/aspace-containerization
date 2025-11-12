09be97972b85f05db18f113f76033516d1f4191f
Samuel Sciolla <ssciolla@umich.edu>
2025-11-12 18:46:41 -0500

    Remove browse_page_db_url from bentley config

--- app/instances/bentley/bentley_config.rb
+++ app/instances/bentley/bentley_config.rb
@@ -18,8 +18,6 @@ AppConfig[:accession_events] = {
   event_types: ["custody_transfer"]
 }
 
-AppConfig[:browse_page_db_url] = ENV["BROWSE_PAGE_DB_URL"]
-
 AppConfig[:user_defined_in_basic] = {
   "accessions" => ["text_1", "string_1", "enum_1", "enum_2", "enum_3", "enum_4", "boolean_1", "boolean_2"],
   "digital_objects" => [],
